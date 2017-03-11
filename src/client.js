61cbf04dbf81d71c70afa41d5fcd8d6b5006a4b9
Átila Camurça <camurca.home@gmail.com>
2017-03-11 10:00:03 -0300

    Profile is changed through notification from gui
    When the user select the desired emulator to run
    a proper profile is notified and the server updates
    all the player keys.
    
    Fixed #10.

--- src/client.js
+++ src/client.js
@@ -2,30 +2,38 @@
 const fs = require('fs')
 const path = require('path')
 
-function Client (id, player) {
-    this.id = id
-    this.player = player
+class Client {
+
+    constructor(id, player) {
+        this.id = id
+        this.player = player
+        this.pad = {
+            type: '',
+            profile: ''
+        }
+    }
+
+    config (settings) {
+        this.pad = {
+            type: settings.pad.type,
+            profile: settings.pad.profile
+        }
+
+        this.setupKeys()
+    }
+
+    setupKeys () {
+        const profilePath = `../profiles/${this.pad.type}/${this.pad.profile}/${this.player}.json`
+        if (fs.existsSync(__dirname + '/' + profilePath)) {
+            this.keys = require(profilePath)
+        } else {
+            this.keys = require(`${__dirname}/../profiles/fallback/nobody.json`)
+            throw `Configuration for user ${this.player} with profile \
+    ${this.pad.type}/${this.pad.profile} not found. Path \
+    '${path.join(__dirname, profilePath)}' does not exists. \
+    Starting in fallback mode, it could not work properly.`
+        }
+    }
 }
-
-/**
- * TODO: settings should be passed by the server itself, not the client!
- */
-Client.prototype.config = function (settings) {
-    this.pad = {
-        type: settings.pad.type,
-        profile: settings.pad.profile
-    }
-
-    const profilePath = `../profiles/${this.pad.type}/${this.pad.profile}/${this.player}.json`
-    if (fs.existsSync(__dirname + '/' + profilePath)) {
-        this.keys = require(profilePath)
-    } else {
-        this.keys = require(`${__dirname}/../profiles/fallback/nobody.json`)
-        throw `Configuration for user ${this.player} with profile \
-${this.pad.type}/${this.pad.profile} not found. Path \
-'${path.join(__dirname, profilePath)}' does not exists. \
-Starting in fallback mode, it could not work properly.`
-    }
-};
 
 module.exports = Client
